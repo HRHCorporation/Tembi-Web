9e9341a87cbcd5a9672da76d099b94aadbc4586a
Aldy Fauzi <aldy.ahmad.ft20@mail.umy.ac.id>
2026-05-09 13:50:13 +0700

    ref: layout admin

--- app/admin/(dashboard)/layout.tsx
+++ app/admin/(dashboard)/layout.tsx
@@ -1,46 +1,3 @@
-// import "@/css/satoshi.css";
-// import "@/css/style.css";
-
-// import "flatpickr/dist/flatpickr.min.css";
-// import "jsvectormap/dist/jsvectormap.css";
-
-// import { Header } from "@/components/admin/layouts/header";
-// import { Sidebar } from "@/components/admin/layouts/sidebar";
-// import { Providers } from "@/app/providers";
-
-// import type { PropsWithChildren } from "react";
-// import NextTopLoader from "nextjs-toploader";
-// import { Toaster } from "sonner";
-
-// export default function AdminLayout({
-//   children,
-// }: PropsWithChildren) {
-//   return (
-//     <Providers>
-//       <NextTopLoader color="#5750F1" showSpinner={false} />
-
-//       <div className="flex min-h-screen">
-//         <Sidebar />
-
-//         <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
-//           <Header />
-
-//           <main className="isolate mx-auto w-full max-w-(--breakpoint-2xl) overflow-hidden p-4 md:p-6 2xl:p-10">
-//             {children}
-//           </main>
-//         </div>
-//       </div>
-
-//       <Toaster
-//         position="bottom-right"
-//         richColors
-//         closeButton
-//         duration={5000}
-//       />
-//     </Providers>
-//   );
-// }
-
 import "@/css/satoshi.css";
 import "@/css/style.css";
 
@@ -66,35 +23,32 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <Providers>
-          <NextTopLoader color="#5750F1" showSpinner={false} />
 
-          <div className="flex min-h-screen">
-            <Sidebar />
+    <Providers>
+      <NextTopLoader color="#5750F1" showSpinner={false} />
 
-            <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
-              <Header />
+      <div className="flex min-h-screen">
+        <Sidebar />
 
-              <main className="isolate mx-auto w-full max-w-(--breakpoint-2xl) overflow-hidden p-4 md:p-6 2xl:p-10">
-                {children}
-              </main>
-            </div>
-          </div>
+        <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
+          <Header />
 
-          <Toaster
-            position="bottom-right"
-            richColors
-            closeButton
-            duration={5000}
-            toastOptions={{
-              className: "dark:bg-gray-dark dark:border-dark-3 dark:text-white",
-            }}
-          />
-        </Providers>
-      </body>
-    </html>
+          <main className="isolate mx-auto w-full max-w-(--breakpoint-2xl) overflow-hidden p-4 md:p-6 2xl:p-10">
+            {children}
+          </main>
+        </div>
+      </div>
+
+      <Toaster
+        position="bottom-right"
+        richColors
+        closeButton
+        duration={5000}
+        toastOptions={{
+          className: "dark:bg-gray-dark dark:border-dark-3 dark:text-white",
+        }}
+      />
+    </Providers>
   );
 }
 
