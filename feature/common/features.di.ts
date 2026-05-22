c54a44d445fedfa12aba5eb3412c4ea4b6949f24
Muhamad Adri Muwaffaq Khamid <muhamad.adri@fli.co.id>
2026-05-22 12:58:56 +0700

    feat: add dependency injection handlers for collection, house, main, and venue modules

--- feature/common/features.di.ts
+++ feature/common/features.di.ts
@@ -5,10 +5,22 @@ import getBannerDi from "../core/banner/data/module/banner.di";
 import di from "@/bootstrap/di/init-di";
 import { foodModuleKey } from "../core/food/food.module-key";
 import getFoodDi from "../core/food/data/module/food.di";
+import { collectionModuleKey } from "../core/collection/collection.module-key";
+import getCollectionDi from "../core/collection/data/module/collection.di";
+import { houseModuleKey } from "../core/house/house.module-key";
+import getHouseDi from "../core/house/data/module/house.di";
+import { mainModuleKey } from "../core/main/main.module-key";
+import getMainDi from "../core/main/data/module/main.di";
+import { venueModuleKey } from "../core/venue/venue.module-key";
+import getVenueDi from "../core/venue/data/module/venue.di";
 
 const moduleKeyToDi: Record<string, (di: DependencyContainer) => DependencyContainer> = {
   [bannerModuleKey]: getBannerDi,
+  [collectionModuleKey]: getCollectionDi,
   [foodModuleKey]: getFoodDi,
+  [houseModuleKey]: getHouseDi,
+  [mainModuleKey]: getMainDi,
+  [venueModuleKey]: getVenueDi
 }
 
 const memoizedDis: Record<string, DependencyContainer> = {};
