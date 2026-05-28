c9f4ec1eeea1a44b3d57dd64b6c6cee3d05e1dc6
Muhamad Adri Muwaffaq Khamid <muhamad.adri@fli.co.id>
2026-05-28 09:44:58 +0700

    Refactor database connection pooling for Laravel and Web to use global variables

--- lib/db-web.ts
+++ lib/db-web.ts
@@ -1,12 +1,22 @@
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-    uri: process.env.DATABASE_URL_WEB,
-    waitForConnections: true,
-    connectionLimit: 10,
-    ssl: {
-        rejectUnauthorized: true,
-    }
-});
+const globalForMySQL = globalThis as unknown as {
+    poolWeb: mysql.Pool | undefined;
+};
+
+const pool =
+    globalForMySQL.poolWeb ??
+    mysql.createPool({
+        uri: process.env.DATABASE_URL_WEB,
+        waitForConnections: true,
+        connectionLimit: 10,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+
+if (process.env.NODE_ENV !== "production") {
+    globalForMySQL.poolWeb = pool;
+}
 
 export default pool;
