2b5a59844349a22b4dc2daa8e6c60e55ce16f6c1
Muhamad Adri Muwaffaq Khamid <muhamad.adri@fli.co.id>
2026-05-29 09:17:07 +0700

    Add remote pattern for staging-compro images in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'staging-compro.tembihistoricalhome.com',
+        port: '3000',
+        pathname: '/**',
+      }
     ],
   },
 };
