caf1b4ab43192bb866742d8db72edc8c22d6f95d
Adri Khamid <muhamad.adri@fli.co.id>
2026-06-16 13:30:16 +0700

    fix: adjust middlewareClientMaxBodySize and update image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   experimental: {
-    middlewareClientMaxBodySize: 300 * 1024 * 1024,
+    middlewareClientMaxBodySize: 100 * 1024 * 1024,
   },
 
   typescript: {
@@ -11,38 +10,20 @@ const nextConfig: NextConfig = {
   },
 
   images: {
-    unoptimized: true,
+    unoptimized: false,
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'http',
-        hostname: '10.241.20.214',
-        port: '3000',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "staging-compro.tembihistoricalhome.com",
+        port: "3000",
+        pathname: "/**",
       },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '3000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'staging-compro.tembihistoricalhome.com',
-        port: '3000',
-        pathname: '/**',
-      }
     ],
   },
 };
