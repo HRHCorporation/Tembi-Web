4973db8ae67e88ecbf2efe7f24de12a48c9b339d
Ubuntu <admintembivps@tembivps.neo.internal>
2026-06-21 22:50:18 +0700

    merge: resolve config ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   experimental: {
-    middlewareClientMaxBodySize: 300 * 1024 * 1024,
+    middlewareClientMaxBodySize: 100 * 1024 * 1024,
   },
 
   typescript: {
@@ -14,42 +13,17 @@ const nextConfig: NextConfig = {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "tembihistoricalhome.com",
         port: "",
         pathname: "/**",
-	},
-	{
-        protocol: 'http',
-        hostname: '10.241.20.214',
-        port: '3000',
-        pathname: '/**',
-	origin/dev-aldy
       },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '3000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'staging-compro.tembihistoricalhome.com',
-        port: '3000',
-        pathname: '/**',
-      }
     ],
   },
 };
